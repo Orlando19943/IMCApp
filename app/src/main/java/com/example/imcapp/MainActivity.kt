3ee35469ee3d1f7acf3d212236f6217b08609fdc
Orlando Cabrera <orlando.osberto@gmail.com>
2020-04-05 22:57:25 -0600

    El codigo el programa ya esta hecho, solamente falta dejarlo bonito :)

--- app/src/main/java/com/example/imcapp/MainActivity.kt
+++ app/src/main/java/com/example/imcapp/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.example.imcapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // get reference to button
+        val btn_click_me = findViewById(R.id.button) as Button
+        // set on-click listener
+        btn_click_me.setOnClickListener {
+            var Peso = txtPeso.text.toString().toDouble()/2.205
+            var Estatura = txtEstatura.text.toString().toDouble()/100
+            var Resultado:Double = Peso/(Estatura*Estatura)
+            var intent = Intent(this,SegundaActividad::class.java)
+            intent.putExtra("Resultado", Resultado)
+            startActivity(intent)
+        }
+        btn_click_me.setOnLongClickListener{
+            Toast.makeText(this, "Formula: Peso (kg) / Estatura² (m)", Toast.LENGTH_SHORT).show()
+            return@setOnLongClickListener true
+        }
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/imcapp/SegundaActividad.kt
@@ -0,0 +1,29 @@
+package com.example.imcapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_segunda_actividad.*
+
+class SegundaActividad : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_segunda_actividad)
+        val objetoIntent: Intent=intent
+        var resultado = objetoIntent.getDoubleExtra("Resultado",0.0)
+        var imc = ""
+        if (resultado < 18.5){
+            imc = "Usted se encuentra por debajo de lo normal :("
+        }else if (resultado > 18.5 && resultado < 25){
+            imc = "Usted se encuentra en lo normal :)"
+        }
+        else if (resultado > 25 && resultado < 30){
+            imc = "Usted se encuentra un poco en sobrepeso -_- :)"
+        }else{
+            imc = "Usted es obeso..."
+        }
+        txtResultado.text = "Su masa corporal es de: $resultado"
+        txtIMC.text = imc
+    }
+}
